8d36f69044647548e207d2420505b9064a4017d6
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-03 14:46:31 +0530

    implemented methods in DataTypeDefinitions and Filter

--- src/com/stackroute/datamunger/query/DataTypeDefinitions.java
+++ src/com/stackroute/datamunger/query/DataTypeDefinitions.java
@@ -1,43 +1,46 @@
 package com.stackroute.datamunger.query;
 
 /*
- * Implementation of DataTypeDefinitions class. This class contains a method getDataTypes() 
+ * Implementation of DataTypeDefinitions class. This class contains a method getDataTypes()
  * which will contain the logic for getting the datatype for a given field value. This
- * method will be called from QueryProcessors.   
- * In this assignment, we are going to use Regular Expression to find the 
- * appropriate data type of a field. 
- * Integers: should contain only digits without decimal point 
- * Double: should contain digits as well as decimal point 
- * Date: Dates can be written in many formats in the CSV file. 
+ * method will be called from QueryProcessors.
+ * In this assignment, we are going to use Regular Expression to find the
+ * appropriate data type of a field.
+ * Integers: should contain only digits without decimal point
+ * Double: should contain digits as well as decimal point
+ * Date: Dates can be written in many formats in the CSV file.
  * However, in this assignment,we will test for the following date formats('dd/mm/yyyy',
  * 'mm/dd/yyyy','dd-mon-yy','dd-mon-yyyy','dd-month-yy','dd-month-yyyy','yyyy-mm-dd')
  */
 public class DataTypeDefinitions {
 
-	//method stub
-	public static Object getDataType(String input) {
-	
-		// checking for Integer
-		
-		// checking for floating point numbers
-		
-		// checking for date format dd/mm/yyyy
-		
-		// checking for date format mm/dd/yyyy
-		
-		// checking for date format dd-mon-yy
-		
-		// checking for date format dd-mon-yyyy
-		
-		// checking for date format dd-month-yy
-		
-		// checking for date format dd-month-yyyy
-		
-		// checking for date format yyyy-mm-dd
-		
-		return null;
-	}
-	
+    //method stub
+    public static String getDataType(String input) {
+        if (input.matches("\\d+")) {
+            return "java.lang.Integer";
+        }
+        // checking for floating point numbers
+        else if (input.matches("\\d+.\\d+")) {
+            return "java.lang.Double";
+        } else if (
+            // checking for date format mm/dd/yyyy
+                input.matches("\\d{2}/\\d{2}/\\d{4}") ||
+                        // checking for date format dd-mon-yy
+                        input.matches("\\d{2}-[aA-zZ]{3}-\\d{2}") ||
+                        // checking for date format dd-mon-yyyy
+                        input.matches("\\d{2}-[aA-zZ]{3}-\\d{4}") ||
+                        // checking for date format dd-month-yy
+                        input.matches("\\d{2}-[aA-zZ]{3,9}-\\d{2}") ||
+                        // checking for date format dd-month-yyyy
+                        input.matches("\\d{2}-[aA-zZ]{3,9}-\\d{4}") ||
+                        // checking for date format yyyy-mm-dd
+                        input.matches("\\d{4}-\\d{2}-\\d{2}")) {
+            return "java.util.Date";
 
-	
+        } else if (input.isEmpty()) {
+            return "java.lang.Object";
+        } else {
+            return "java.lang.String";
+        }
+    }
 }
